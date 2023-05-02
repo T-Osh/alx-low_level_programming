3e67466eb63f1ff08e0b99dfcd8fba4dcc6857a5
Tomilayo Osho <oshotomilayo@gmail.com>
2023-05-02 12:09:03 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+redadme file for ALX 0x05-pointers_arrays_strings C programming tasks using _putchar
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 0-reset_to_98.c task */
+void reset_to_98(int *n);
+
+/* 1-swap.c task */
+void swap_int(int *a, int *b);
+
+/* 2-strlen.c task */
+int _strlen(char *s);
+
+/* 3-puts.c task */
+void _puts(char *str);
+
+/* 4-print_rev.c task */
+void print_rev(char *s);
+
+/* 5-rev_string.c task */
+void rev_string(char *s);
+
+/* 6-puts2.c task */
+void puts2(char *str);
+
+/* 7-puts_half.c task */
+void puts_half(char *str);
+
+/* 8-print_array.c task */
+void print_array(int *a, int n);
+
+/* 9-strcpy.c task */
+char *_strcpy(char *dest, char *src);
+
+/* 100-atoi.c task */
+int _atoi(char *s);
+
+/* _putchar.c */
+int _putchar(char c);
+
+#endif

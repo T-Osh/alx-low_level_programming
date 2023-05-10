6e6e9a0f1c7a2af56f52669037c52e4095d703d2
Tomilayo Osho <oshotomilayo@gmail.com>
2023-05-10 09:31:49 -0700

    squareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,9 @@
 */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (_sqrt(n, 0));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
 }
 /**
  * _sqrt - find the natural
